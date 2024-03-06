bfe82f0f6bfdc3cc40e90d9286d669dc8e4a705f
Ben Cuff <156725385+ben-cuff@users.noreply.github.com>
2024-03-06 05:54:58 +0000

    added a button and a games page

---
+++ redirectToGames.js
@@ -0,0 +1,6 @@
+var button = document.getElementById('myButton');
+// Add a click event listener to the button
+button.addEventListener('click', function () {
+    // Redirect the user to a different URL when the button is clicked
+    window.location.href = '/games';
+});
---
+++ redirectToGames.ts
@@ -0,0 +1,7 @@
+let button = document.getElementById('myButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+    // Redirect the user to a different URL when the button is clicked
+    window.location.href = '/games';
+});
