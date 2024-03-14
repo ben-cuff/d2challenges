74cd43ead36f7373110fb0c8faf87fb358b553aa
Ben Cuff <156725385+ben-cuff@users.noreply.github.com>
2024-03-14 01:19:57 +0000

    removed redirects directory

--- redirects/redirectToGames.ts
+++
@@ -1,7 +0,0 @@
-let button = document.getElementById('myButton');
-
-// Add a click event listener to the button
-button.addEventListener('click', () => {
-    // Redirect the user to a different URL when the button is clicked
-    window.location.href = '/games';
-});
